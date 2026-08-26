156c82b632a9cca927ea4b28b401982113c7bd39
GCSG01 <GCSG01@outlook.com>
2026-08-26 21:00:09 +0800

    update

--- P2541.cpp
+++ P2541.cpp
@@ -1,21 +1,39 @@
 #include<bits/stdc++.h>
 #define int long long
 using namespace std;
-const int N=2e5+5;
-int n,k,m[N],p[N][12];
-priority_queue<pair<int,pair<int,int>>>pq;
+const int N=1e5+5,inf=2e18;
+struct cow{
+	int len,d[20],v;
+}p[N];
+struct node{
+	int v,x,y;
+};
+int n,k,ans;
+priority_queue<node>q;
+bool operator<(node u,node v){return u.v>v.v;}
 signed main(){
     ios::sync_with_stdio(0);cin.tie(0);
-    cin>>n>>k;
+	cin>>n>>k;
     for(int i=1;i<=n;i++){
-        cin>>m[i];
-        for(int j=1;j<=m[i];j++)
-            cin>>p[i][j];
-        sort(p[i]+1,p[i]+m[i],1);
-    }
-    int ans=0;
-    for(int i=1;i<=n;i++)
-        pq.push({p[i][1],{i,1}});
-    for(int i=1;i<=k;i++){}
-    return 0;
+		cin>>p[i].len;
+		for(int j=1;j<=p[i].len;j++)
+            cin>>p[i].d[j];
+		p[i].d[p[i].len+1]=inf;
+		sort(p[i].d+1,p[i].d+p[i].len+1,[](int A,int B){return A<B;});
+		p[i].v=p[i].d[2]-p[i].d[1],ans+=p[i].d[1];
+	}
+	sort(p+1,p+n+1,[](cow A,cow B){return A.v<B.v;});
+	q.push({ans-p[1].d[1]+p[1].d[2],1,2});
+	for(int i=1;i<k;i++){
+		node u=q.top();q.pop();
+        auto [v,x,y]=u;
+		ans+=v;
+		q.push({v-p[x].d[y]+p[x].d[y+1],x,y+1});
+		if(x!=n)
+			q.push({v-p[x+1].d[1]+p[x+1].d[2],x+1,2});
+		if(x!=n&&y==2)
+			q.push({v-p[x+1].d[1]+p[x+1].d[2]-p[x].d[2]+p[x].d[1],x+1,2});
+	}
+    cout<<ans;
+	return 0;
 }
